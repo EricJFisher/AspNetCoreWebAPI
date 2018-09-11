4781ea93900ed7ef986183eb29d26629c541d21b
Eric J Fisher <eric-fisher@pluralsight.com>
2018-09-11 11:28:28 -0400

    4.3 Completed

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
 
 namespace StarChart.Controllers
 {
@@ -10,5 +11,6 @@ namespace StarChart.Controllers
     [ApiController]
     public class CelestialObjectController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
     }
 }
