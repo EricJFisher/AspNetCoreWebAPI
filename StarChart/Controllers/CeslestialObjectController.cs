c2993b40e3ce4945dc41c37dd22d325f78b53ef2
bourne17 <eduar2.mariano@gmail.com>
2021-07-20 10:39:50 -0400

    add Controller

---
+++ StarChart/Controllers/CeslestialObjectController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class CeslestialObjectController:ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public CeslestialObjectController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+        
+
+    }
+}
